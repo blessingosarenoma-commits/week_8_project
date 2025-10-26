e84b8989c5cabc845b5c283bad1a6b5e6893094f
Osarenoma Blessing Dick <blessingosarenoma@gmail.com>
2025-10-26 16:56:14 +0000

    comment blog

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react'
+import {MessageCircle, User, Calendar, ArrowLeft, Send} from 'react'
+const INITIAL_POSTS =[
+    {
+        id: 1,
+        author: 'Blessing',
+        content: 'Animals',
+        timestamp: '3 hours ago',
+        likes: 10
+
+    },
+
+    {
+
+        id: 2,
+        author:'Blessing',
+        content: 'wild animals',
+        timestamp: '4 hours ago',
+        likes: 20,
+
+    },
+
+    const [newComment, setNewComment] =useState("),
+]
